557d6fb6a7a119347a2bf2c57183c0843bbee7b1
gauravjsh8 <gauravjsh8@gmail.com>
2025-06-05 21:02:28 +0300

    add getAllJobs functionality

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -38,3 +38,30 @@ export const createJobs = async (req, res) => {
     return res.status(500).json({ success: false, message: "Server Error" });
   }
 };
+
+export const getAllJobs = async (req, res) => {
+  try {
+    const { jobType, company, location, page = 1, limit = 10 } = req.query;
+
+    const filter = {};
+    if (jobType) filter.jobType = jobType;
+    if (company) filter.company = company;
+    if (location) filter.location = location;
+
+    const allJobs = await Job.find(filter)
+      .populate("postedBy", "name email")
+      .limit(Number(limit))
+      .skip((page - 1) * limit)
+      .sort({ createdAt: -1 });
+    const total = await Job.countDocuments(filter);
+    res.json({
+      total,
+      page: Number(page),
+      limit: Number(limit),
+      jobs: allJobs,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 import { authUser } from "../middleware/authMiddleware.js";
-import { createJobs } from "../controllers/jobController.js";
+import { createJobs, getAllJobs } from "../controllers/jobController.js";
 
 export const jobRouter = express.Router();
 jobRouter.post("/create-job", authUser, createJobs);
+jobRouter.get("/getalljobs", getAllJobs);
