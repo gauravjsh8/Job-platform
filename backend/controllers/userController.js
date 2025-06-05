c65a2667b442015ad2f57bfd236e44dc5eb62aaf
gauravjsh8 <gauravjsh8@gmail.com>
2025-06-05 19:00:38 +0300

    add login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,10 +37,63 @@ export const createUser = async (req, res) => {
       photoUrl,
     });
 
+    const userResponse = newUser.toObject();
+
+    delete userResponse.password;
+
     return res.status(201).json({
       success: true,
       message: "User registered successfully",
-      newUser: newUser,
+      newUser: userResponse,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+
+    if (!email || !password || !role) {
+      return res
+        .status(403)
+        .json({ success: false, message: "All fields are required" });
+    }
+
+    const existingUser = await User.findOne({ email }).select("+password");
+    if (!existingUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User is not registered with this email.",
+      });
+    }
+
+    if (existingUser.role !== role) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid role provided.",
+      });
+    }
+    const isPasswordMatch = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordMatch) {
+      return res.status(403).json({
+        success: false,
+        message: "Invalid Password.",
+      });
+    }
+
+    const userResponse = existingUser.toObject();
+
+    delete userResponse.password;
+    return res.status(200).json({
+      success: true,
+      message: "User loggedin successfully.",
+      user: userResponse,
     });
   } catch (error) {
     console.error(error);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, loginUser } from "../controllers/userController.js";
 import upload from "../middleware/multer.js";
 
 export const userRouter = express.Router();
 userRouter.post("/register", upload.single("photo"), createUser);
+userRouter.post("/login", loginUser);
