2ee8565446836718ed58cecb39bcedcc278e137c
gauravjsh8 <gauravjsh8@gmail.com>
2025-06-11 12:49:09 +0300

    add footer functionality

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-cyan-500 text-center text-gray-600 py-4  ">
+    <footer className="bg-cyan-500 text-center text-white py-4  ">
       <p>&copy; {new Date().getFullYear()} CareerLink. All rights reserved.</p>
     </footer>
   );
